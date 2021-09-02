7d8f8379d80e2a10ba85ff509cee129191f108fe
Vasu Chitte <vasuc@google.com>
2021-09-02 08:51:44 +0000

    feat: development

---
+++ datasets/bls/_images/run_csv_transform_kub_unemployment_cps_series/csv_transform.py
@@ -0,0 +1,138 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+import pathlib
+
+import pandas as pd
+import requests
+from google.cloud import storage
+
+
+def main(
+    source_url: str,
+    source_file: pathlib.Path,
+    target_file: pathlib.Path,
+    target_gcs_bucket: str,
+    target_gcs_path: str,
+):
+
+    logging.info("creating 'files' folder")
+    pathlib.Path("./files").mkdir(parents=True, exist_ok=True)
+    
+    logging.info(f"Downloading file {source_url}")
+    download_file(source_url, source_file)
+
+    logging.info(f"Opening file {source_file}")
+   
+    df=pd.read_csv(str({source_file}), sep="\t")
+
+    logging.info(f"Transforming.. {source_file}")
+    trim_whitespaces(df)
+    logging.info("Transform: Reordering headers..")
+    df = df[    
+        [
+            "series_id",
+            "lfst_code",
+            "periodicity_code",
+            "series_title",
+            "absn_code",
+            "activity_code",
+            "ages_code",
+            "class_code",
+            "duration_code",
+            "education_code",
+            "entr_code"
+            "expr_code",
+            "hheader_code",
+            "hour_code",
+            "indy_code",
+            "jdes_code",
+            "look_code",
+            "mari_code",
+            "mjhs_code",
+            "occupation_code",
+            "orig_code",
+            "pcts_code"
+            "race_code",
+            "rjnw_code",
+            "rnlf_code",
+            "rwns_code",
+            "seek_code",
+            "sexs_code",
+            "tdat_code",
+            "vets_code",
+            "wkst_code",
+            "born_code",
+            "chld_code"
+            "disa_code",
+            "seasonal",
+            "footnote_codes",
+            "begin_year",
+            "begin_period",
+            "end_year",
+            "end_period",
+            "cert_code"
+        ]
+    ]
+
+    logging.info(f"Saving to output file.. {target_file}")
+    try:
+        save_to_new_file(df, file_path=str(target_file))
+    except Exception as e:
+        logging.error(f"Error saving output file: {e}.")
+    logging.info("..Done!")
+
+    logging.info(
+        f"Uploading output file to.. gs://{target_gcs_bucket}/{target_gcs_path}"
+     )
+    upload_file_to_gcs(target_file, target_gcs_bucket, target_gcs_path)
+
+def trim_whitespaces(df):
+    cols = ["series_id","footnote_codes","series_title"]
+    df[cols] = df[cols].apply(lambda x: x.str.strip())
+    return df[cols]
+
+def save_to_new_file(df, file_path):
+    df.export_csv(file_path)
+
+def download_file(source_url: str, source_file: pathlib.Path):
+    logging.info(f"Downloading {source_url} into {source_file}")
+    r = requests.get(source_url, stream=True)
+    if r.status_code == 200:
+        with open(source_file, "wb") as f:
+            for chunk in r:
+                f.write(chunk)
+    else:
+        logging.error(f"Couldn't download {source_url}: {r.text}")
+
+
+def upload_file_to_gcs(file_path: pathlib.Path, gcs_bucket: str, gcs_path: str) -> None:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(gcs_bucket)
+    blob = bucket.blob(gcs_path)
+    blob.upload_from_filename(file_path)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+
+    main(
+        source_url=os.environ["SOURCE_URL"],
+        source_file=pathlib.Path(os.environ["SOURCE_FILE"]).expanduser(),
+        target_file=pathlib.Path(os.environ["TARGET_FILE"]).expanduser(),
+        target_gcs_bucket=os.environ["TARGET_GCS_BUCKET"],
+        target_gcs_path=os.environ["TARGET_GCS_PATH"],
+    )
